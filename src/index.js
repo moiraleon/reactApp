9cba425db93dba4c7a51cd35c208c70dad455586
Moira Leon <moira.leon2@gmail.com>
2022-02-24 18:35:25 -0700

    adding filter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from "react";
+// import React from "react";
 
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 
-const element = <h1>Hello World</h1>;
+// const element = <h1>Hello World</h1>;
 
-ReactDOM.render(element, document.getElementById('root'));
+// ReactDOM.render(element, document.getElementById('root'));
+
+const jobs = [
+    { id: 1, isActive: true },
+    { id: 2, isActive: true },
+    { id: 3, isActive: false },
+]
+    const activeJobs = jobs.filter(function(job) { return job.isActive; })``;
+    ``
